0949fde9f32ee9e56ceca4673c87b8dfaab08ed2
ljz2009y <773960716@qq.com>
2014-03-01 10:34:19 +0800

    HelloGithub

---
+++ src/com/hello/ljz/HelloGitHub.java
@@ -0,0 +1,13 @@
+package com.hello.ljz;
+
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github");
+	}
+
+}
